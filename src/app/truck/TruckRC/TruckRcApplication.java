27ab71575052e632a2df0709e968ab59d132c605
Thomas Martitz <thomas.martitz@student.htw-berlin.de>
2011-05-04 09:23:43 +0200

    Clarify license and add according documents.
    Source distribution license is GPLv2 or later, except for the
    app.truck.TruckRC.orientation component which is GPLv3. Therefore binary
    distribution is GPLv3.

--- src/app/truck/TruckRC/TruckRcApplication.java
+++ src/app/truck/TruckRC/TruckRcApplication.java
@@ -1,3 +1,34 @@
+/*
+ *  ___           ____
+ * |   |         |###|
+ * |___|___      |###|___     ___     ___     ___
+ *     |    \    |#######|   |###|   |###|   |###|
+ *  ___|___   \  |###____|   |###|   |###|   |###|
+ * |   |   |   | |###|       |###|   |###|   |###|
+ * |   |   |   | |### \      |####\  |###|  /####|___
+ * |   |   |   |  \######|    \##############|   |   |
+ * |___|   |___|    \####|      \############|   |___|
+ *
+ * Hochschule für Technik und Wirtschaft Berlin
+ * University of Appplied Sciences
+ *
+ * Copyright 2011 Thomas Martitz
+ *
+ * 
+ * TruckRC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * TruckRC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TruckRC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package app.truck.TruckRC;
 
 import java.util.ArrayList;
